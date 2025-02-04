fe86bd708d08bf3ca69585ac79c04e523d6a9a73
NhatBangLe <bangle37568@gmail.com>
2025-02-04 19:59:36 +0700

    Downgrade react-router-dom version to v6

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './i18n/i18n.ts';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
 import RootLayout from './layouts/RootLayout.tsx';
 import {
 	CreatDeployDocumentPage,
@@ -26,73 +31,69 @@ import { store } from './redux/store.ts';
 //import CreateModifyVersionForm from './components/CreateModifyVersionForm.tsx';
 import ExamplePage from './pages/test/index.tsx';
 
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route
+				element={<RootLayout />}
+				errorElement={<ErrorPage />}
+				hydrateFallbackElement={<HydrateFallback />}
+			>
+				<Route path="overview" element={<OverviewPage />} />
+				<Route path="product" element={<ProductManagementPage />} />
+				<Route path="/create-product" element={<CreateProductPage />} />
+				<Route
+					path="/product-version/:version"
+					element={<ProductVersionDetail />}
+				/>
+				<Route
+					path="/product/:productId/create-version"
+					element={<CreateVersionProductPage />}
+				/>
+				<Route path="/modify-product/:id" element={<ModifyProductPage />} />
+				<Route path="deploy" element={<DeployManagementPage />} />
+				<Route
+					path="/create-deploy-document"
+					element={<CreatDeployDocumentPage />}
+				/>
+				<Route path="document">
+					<Route path="label" element={<DocumentLabelPage />} />
+					<Route
+						path="product-design"
+						element={<ProductDesignDocumentPage />}
+					/>
+
+					<Route path="module-design" element={<ModuleDesignDocumentPage />} />
+				</Route>
+				<Route
+					path="/create-product-design-document"
+					element={<CreatProductDesignDocumentPage />}
+				/>
+				<Route
+					path="/create-module-design-document"
+					element={<CreatModuleDesignDocumentPage />}
+				/>
+				{/* <Route
+					path="test"
+					element={
+						<CreateModifyVersionForm
+							label="hahaha"
+							title="ssss"
+							onSubmit={() => App}
+							onCancel={App}
+						/>
+					}
+				/> */}
+				<Route path="test" element={<ExamplePage />} />
+			</Route>
+		</Route>
+	)
+);
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<App />}>
-						<Route
-							element={<RootLayout />}
-							errorElement={<ErrorPage />}
-							hydrateFallbackElement={<HydrateFallback />}
-						>
-							<Route path="overview" element={<OverviewPage />} />
-							<Route path="product" element={<ProductManagementPage />} />
-							<Route path="/create-product" element={<CreateProductPage />} />
-							<Route
-								path="/product-version/:version"
-								element={<ProductVersionDetail />}
-							/>
-							<Route
-								path="/product/:productId/create-version"
-								element={<CreateVersionProductPage />}
-							/>
-							<Route
-								path="/modify-product/:id"
-								element={<ModifyProductPage />}
-							/>
-							<Route path="deploy" element={<DeployManagementPage />} />
-							<Route
-								path="/create-deploy-document"
-								element={<CreatDeployDocumentPage />}
-							/>
-							<Route path="document">
-								<Route path="label" element={<DocumentLabelPage />} />
-								<Route
-									path="product-design"
-									element={<ProductDesignDocumentPage />}
-								/>
-
-								<Route
-									path="module-design"
-									element={<ModuleDesignDocumentPage />}
-								/>
-							</Route>
-							<Route
-								path="/create-product-design-document"
-								element={<CreatProductDesignDocumentPage />}
-							/>
-							<Route
-								path="/create-module-design-document"
-								element={<CreatModuleDesignDocumentPage />}
-							/>
-							{/* <Route
-								path="test"
-								element={
-									<CreateModifyVersionForm
-										label="hahaha"
-										title="ssss"
-										onSubmit={() => App}
-										onCancel={App}
-									/>
-								}
-							/> */}
-							<Route path="test" element={<ExamplePage />} />
-						</Route>
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<RouterProvider router={router} />
 		</Provider>
-	</StrictMode>,
+	</StrictMode>
 );
